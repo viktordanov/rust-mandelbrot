1eaffd1dbf2c1a61ad495b41dcfbbf47edd82947
Viktor Danov <orangeleaf12@gmail.com>
2022-10-01 01:55:47 +0300

    clean up color code

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -13,30 +13,13 @@ pub fn get_color_at(x: f64, y: f64, max_iterations: u32) -> (u8, u8, u8) {
     if i == max_iterations {
         (0, 0, 0)
     } else {
-		let V = (i as f64).ln();
+		let v = (i as f64).ln();
 
-		// (255,255,255) * (1 + cos(2 * pi * (V - 0.5))) / 2
-
-		let hsv = Hsv::new(360.0 * (1.0 + (V).cos()) / 2.0, 1.0, 1.0);
+		let hsv = Hsv::new(360.0 * (1.0 + (v).cos()) / 2.0, 1.0, 1.0);
 		let rgb = Srgb::from_hsv(hsv);
 		let r = (rgb.red * 255.0) as u8;
 		let g = (rgb.green * 255.0) as u8;
 		let b = (rgb.blue * 255.0) as u8;
 		(r, g, b)
-	
-
-
-
-		// let smooth = i as f64 + 1_f64 - (z.norm().ln().log2() / 2.0_f64.ln());
-			  
-		// let hsb = (smooth * 6.0 as f64, 0.7, 0.1 + min(smooth*smooth/max_iterations as f64, 0.86)); 
-		// let hsb = Hsv::new(hsb.0, hsb.1, hsb.2); 
-		// let srgb = Srgb::from_hsv(hsb);
-		  
-		//   (
-		// 	  (srgb.red * 255.0) as u8,
-		// 	  (srgb.green * 255.0) as u8,
-		// 	  (srgb.blue * 255.0) as u8,
-		//   )
     }
 }
